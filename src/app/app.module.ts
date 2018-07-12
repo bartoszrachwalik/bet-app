94b1d01cde409bb814003aea788844f8fddce4c8
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-07-12 11:59:38 +0200

    adds navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-
-import { AppComponent } from './app.component';
-
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {HeaderComponent} from './header/header.component';
+import {RankingComponent} from './ranking/ranking.component';
+import {MyBetsComponent} from './my-bets/my-bets.component';
+import {AppRoutingModule} from './ranking/app-routing.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    RankingComponent,
+    MyBetsComponent,
+    PageNotFoundComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/ranking/app-routing.module.ts
@@ -0,0 +1,26 @@
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+
+import {HomeComponent} from '../home/home.component';
+import {MyBetsComponent} from '../my-bets/my-bets.component';
+import {RankingComponent} from './ranking.component';
+import {PageNotFoundComponent} from '../page-not-found/page-not-found.component';
+import {SigninComponent} from '../auth/signin/signin.component';
+import {SignupComponent} from '../auth/signup/signup.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'my-bets', component: MyBetsComponent},
+  {path: 'ranking', component: RankingComponent},
+  {path: 'signin', component: SigninComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: '**', component: PageNotFoundComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+}
